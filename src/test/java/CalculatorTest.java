7aa23491e2e22b820fc0c1f8453021c90816729e
Dante de Araújo Costa <66438227+danteacosta@users.noreply.github.com>
2022-10-19 14:17:58 -0300

    commit com estrutura base de junit e maven project

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+public class CalculatorTest {
+    @Test
+    @DisplayName("Add two numbers")
+    void add() {
+        assertEquals(4, Calculator.add(2, 2));
+    }
+}
